be98cfb66ccd52fbe8055446294672a297a46cab
AlexChihaia <alexchihaia84@gmail.com>
2024-01-09 10:41:39 +0100

    feat:  ireplace method

--- execute.php
+++ execute.php
@@ -4,7 +4,7 @@ $paragrafo = $_GET["paragrafo"];
 $parola = $_GET["parola"];
 echo "Paragrafo originale:<br>";
 echo $paragrafo; echo "<br>Lunghezza: " . strlen($paragrafo) . " caratteri<br>";
-$paragrafoCensurato = str_replace($parola, "***", $paragrafo);
+$paragrafoCensurato = str_ireplace($parola, "***", $paragrafo);
 echo "Paragrafo censurato:<br>";
 echo $paragrafoCensurato;
 echo "<br>Lunghezza: " . strlen($paragrafoCensurato) . " caratteri<br>"; 
